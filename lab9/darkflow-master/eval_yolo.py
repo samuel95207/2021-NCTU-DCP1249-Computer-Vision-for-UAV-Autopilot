f044e0a32c6d030d55e29e128282987cc707fdb2
Samuel Huang <lspss95207@gmail.com>
2021-05-28 15:50:39 +0800

    Update eval_yolo.py to output video file

--- lab9/darkflow-master/eval_yolo.py
+++ lab9/darkflow-master/eval_yolo.py
@@ -2,17 +2,17 @@ from darkflow.net.build import TFNet
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import os 
+import os
 
-threshold = 0.5
-options = {"pbLoad": "../result/0508_carna_2/yolov2-tiny-voc-lab9.pb",
-            "threshold": threshold,
-           "metaLoad": "../result/0508_carna_2/yolov2-tiny-voc-lab9.meta" 
+threshold = 0.09
+options = {"pbLoad": "../result/0518_4/yolov2-tiny-voc-lab9.pb",
+           "threshold": threshold,
+           "metaLoad": "../result/0518_4/yolov2-tiny-voc-lab9.meta",
+           "GPU": 1.0
            }
-eval_path = 'test_img/'         
+eval_path = 'test_img/'
 
 tfnet = TFNet(options)
-
 
 
 def boxing(original_img, predictions, thre):
@@ -29,12 +29,12 @@ def boxing(original_img, predictions, thre):
         label = result['label'] + " " + str(round(confidence, 2))
 
         if confidence > thre:
-            newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_SIMPLEX ,1, (0, 255, 0), 1, cv2.LINE_AA)
-            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
-            
-            
-    return newImage
+            newImage = cv2.putText(newImage, label, (top_x, top_y-5),
+                                   cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+            newImage = cv2.rectangle(
+                newImage, (top_x, top_y), (btm_x, btm_y), (255, 0, 0), 3)
 
+    return newImage
 
 
 # images_ls = [f for f in os.listdir(eval_path) if f.endswith('.jpg')]
@@ -50,24 +50,35 @@ def boxing(original_img, predictions, thre):
 #     else:
 #         print("Nothing detected")
 
-
 cap = cv2.VideoCapture('../demo_video/demo.wmv')
+
+cap_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+cap_fps = cap.get(cv2.CAP_PROP_FPS)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+outfile = cv2.VideoWriter('output.mp4', fourcc, cap_fps, cap_size)
+
 
 while True:
     ret, imgcv = cap.read()
-    result = tfnet.return_predict(imgcv)
 
     if(not ret):
         print("Video end")
         break
 
-    if len(result)!=0:
-        cv2.imshow("result", boxing(imgcv, result, threshold))
+    result = tfnet.return_predict(imgcv)
+
+    if len(result) != 0:
+        imgcv = boxing(imgcv, result, threshold)
     else:
-        cv2.imshow("result", imgcv)
         print("Nothing detected")
+
+    cv2.imshow("result", imgcv)
+    outfile.write(imgcv)
 
     cv2.waitKey(10)
 
+cap.release()
+outfile.release()
 
-
+cv2.destroyAllWindows()
